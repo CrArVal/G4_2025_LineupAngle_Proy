0e0a754d243dc0f92432e53e996af4f376708046
JM1146-AROHBKNIGHT <jm1146945@gmail.com>
2025-10-21 16:33:55 -0300

    listo perfil sova y href de lineup home a todos los personajes
    falta agregar menu e lineups de cada personaje, y si ver si es que se agregan mas lineups a sova

--- lineup/urls.py
+++ lineup/urls.py
@@ -4,6 +4,36 @@ from . import views
 urlpatterns = [
     path('lineuphome/', views.lineuphome, name='lineuphome'),
     path('lineupsovahave/', views.lineupsovahave, name='lineupsovahave'),
+    path('gekkolineup/', views.gekkolineup, name='gekkolineup'),
+    path('breachlineup/', views.breachlineup, name='breachlineup'),
+    path('kayolineup/', views.kayolineup, name='kayolineup'),
+    path('phoenixlineup/', views.phoenixlineup, name='phoenixlineup'),
+    path('neonlineup/', views.neonlineup, name='neonlineup'),
+    path('reynalineup/', views.reynalineup, name='reynalineup'),
+    path('clovelineup/', views.clovelineup, name='clovelineup'),
+    path('tejolineup/', views.tejolineup, name='tejolineup'),
+    path('vetolineup/', views.vetolineup, name='vetolineup'),
     
+    # Columna 2
+    path('harborlineup/', views.harborlineup, name='harborlineup'),
+    path('razelineup/', views.razelineup, name='razelineup'),
+    path('skyelineup/', views.skyelineup, name='skyelineup'),
+    path('killjoylineup/', views.killjoylineup, name='killjoylineup'),
+    path('astralineup/', views.astralineup, name='astralineup'),
+    path('yorulineup/', views.yorulineup, name='yorulineup'),
+    path('omenlineup/', views.omenlineup, name='omenlineup'),
+    path('deadlocklineup/', views.deadlocklineup, name='deadlocklineup'),
+    path('waylaylineup/', views.waylaylineup, name='waylaylineup'),
+    
+    # Columna 3
+    path('fadelineup/', views.fadelineup, name='fadelineup'),
+    path('chamberlineup/', views.chamberlineup, name='chamberlineup'),
+    path('cypherlineup/', views.cypherlineup, name='cypherlineup'),
+    path('viperlineup/', views.viperlineup, name='viperlineup'),
+    path('brimstonelineup/', views.brimstonelineup, name='brimstonelineup'),
+    path('sagelineup/', views.sagelineup, name='sagelineup'),
+    path('jettlineup/', views.jettlineup, name='jettlineup'),
+    path('isolineup/', views.isolineup, name='isolineup'),
+    path('vyselineup/', views.vyselineup, name='vyselineup'),
     
 ]
--- lineup/views.py
+++ lineup/views.py
@@ -7,3 +7,88 @@ def lineuphome(request):
     return render(request, 'lineup/lineuphome.html')
 def lineupsovahave(request):
     return render(request, 'lineup/lineupsovahave.html')
+# Columna 1
+def gekkolineup(request):
+    return render(request, 'lineup/gekkolineup.html')
+
+def breachlineup(request):
+    return render(request, 'lineup/breachlineup.html')
+
+def kayolineup(request):
+    return render(request, 'lineup/kayolineup.html')
+
+def phoenixlineup(request):
+    return render(request, 'lineup/phoenixlineup.html')
+
+def neonlineup(request):
+    return render(request, 'lineup/neonlineup.html')
+
+def reynalineup(request):
+    return render(request, 'lineup/reynalineup.html')
+
+def clovelineup(request):
+    return render(request, 'lineup/clovelineup.html')
+
+def tejolineup(request):
+    return render(request, 'lineup/tejolineup.html')
+
+def vetolineup(request):
+    return render(request, 'lineup/vetolineup.html')
+
+
+# Columna 2
+def harborlineup(request):
+    return render(request, 'lineup/harborlineup.html')
+
+def razelineup(request):
+    return render(request, 'lineup/razelineup.html')
+
+def skyelineup(request):
+    return render(request, 'lineup/skyelineup.html')
+
+def killjoylineup(request):
+    return render(request, 'lineup/killjoylineup.html')
+
+def astralineup(request):
+    return render(request, 'lineup/astralineup.html')
+
+def yorulineup(request):
+    return render(request, 'lineup/yorulineup.html')
+
+def omenlineup(request):
+    return render(request, 'lineup/omenlineup.html')
+
+def deadlocklineup(request):
+    return render(request, 'lineup/deadlocklineup.html')
+
+def waylaylineup(request):
+    return render(request, 'lineup/waylaylineup.html')
+
+
+# Columna 3
+def fadelineup(request):
+    return render(request, 'lineup/fadelineup.html')
+
+def chamberlineup(request):
+    return render(request, 'lineup/chamberlineup.html')
+
+def cypherlineup(request):
+    return render(request, 'lineup/cypherlineup.html')
+
+def viperlineup(request):
+    return render(request, 'lineup/viperlineup.html')
+
+def brimstonelineup(request):
+    return render(request, 'lineup/brimstonelineup.html')
+
+def sagelineup(request):
+    return render(request, 'lineup/sagelineup.html')
+
+def jettlineup(request):
+    return render(request, 'lineup/jettlineup.html')
+
+def isolineup(request):
+    return render(request, 'lineup/isolineup.html')
+
+def vyselineup(request):
+    return render(request, 'lineup/vyselineup.html')
